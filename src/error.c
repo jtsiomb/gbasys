4e86e490730ac4ceee75dc59343a500ce36a0340
John Tsiombikas <nuclear@member.fsf.org>
2016-02-01 02:36:25 +0200

    added panicf

--- src/error.c
+++ src/error.c
@@ -1,6 +1,6 @@
 /*
 gbasys - a gameboy advance hardware abstraction library
-Copyright (C) 2004-2014  John Tsiombikas <nuclear@member.fsf.org>
+Copyright (C) 2004-2016  John Tsiombikas <nuclear@member.fsf.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,6 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <stdio.h>
+#include <stdarg.h>
 #include "gfx.h"
 
 void panic(const char *str) {
@@ -27,4 +29,16 @@ void panic(const char *str) {
 	if(str) draw_string(str, 0, 32, front_buffer);
 
 	for(;;);
+}
+
+void panicf(const char *fmt, ...)
+{
+	static char buf[64];
+	va_list ap;
+
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof buf - 1, fmt, ap);
+	va_end(ap);
+
+	panic(buf);
 }
--- src/error.h
+++ src/error.h
@@ -1,6 +1,6 @@
 /*
 gbasys - a gameboy advance hardware abstraction library
-Copyright (C) 2004-2014  John Tsiombikas <nuclear@member.fsf.org>
+Copyright (C) 2004-2016  John Tsiombikas <nuclear@member.fsf.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,5 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _ERROR_H_
 
 void panic(const char *str);
+void panicf(const char *fmt, ...);
 
 #endif	/* _ERROR_H_ */
